162fdd533801aa4e26bfc28abbd2d2bc23fb3a03
Andrej Kenda <andrej.baca14@gmail.com>
2023-04-01 14:39:27 +0200

    updated layout saving with window options

--- lua/db/layout.lua
+++ lua/db/layout.lua
@@ -1,23 +1,44 @@
 -- save and restore window layout
 
----@alias layoutEgg { layout: any, restore: any }
+---@alias _layout { type: string, winid: integer, bufnr: integer, win_opts: { string: any}, children: _layout[] }
 
--- layout structure:
--- { "row", { { "leaf", winid, bufnr }, { "col", { { "leaf", winid, bufnr }, { "leaf", winid, bufnr } } } } }
+---@alias layoutEgg { layout: _layout, restore: any }
+
+-- vim.fn.winlayout() example structure:
+-- { "row", { { "leaf", winid }, { "col", { { "leaf", winid }, { "leaf", winid } } } } }
 
 local M = {}
 
 -- add bufnr to leaf
-local function add_buf_to_layout(layout)
+local function add_details(layout)
   if layout[1] == "leaf" then
-    table.insert(layout, vim.fn.winbufnr(layout[2]))
-    return layout
+    local win = layout[2]
+
+    -- window options
+    local all_options = vim.api.nvim_get_all_options_info()
+    local v = vim.wo[win]
+    local options = {}
+    for key, val in pairs(all_options) do
+      if val.global_local == false and val.scope == "win" then
+        options[key] = v[key] or nil
+      end
+    end
+
+    -- create dict structure with added buffer and window opts
+    ---@type _layout
+    local l = {
+      type = layout[1],
+      winid = win,
+      bufnr = vim.fn.winbufnr(win),
+      win_opts = options,
+    }
+    return l
   else
     local children = {}
     for _, child_layout in ipairs(layout[2]) do
-      table.insert(children, add_buf_to_layout(child_layout))
+      table.insert(children, add_details(child_layout))
     end
-    return { layout[1], children }
+    return { type= layout[1], children = children }
   end
 end
 
@@ -26,26 +47,35 @@ function M.save()
   local layout = vim.fn.winlayout()
   local restore_cmd = vim.fn.winrestcmd()
 
-  local buf_layout = add_buf_to_layout(layout)
+  layout = add_details(layout)
+  vim.pretty_print(layout)
 
-  return { layout = buf_layout, restore = restore_cmd }
+  return { layout = layout, restore = restore_cmd }
 end
 
+---@param layout _layout
 local function apply_layout(layout)
-  if layout[1] == "leaf" then
-    if vim.fn.bufexists(layout[3]) == 1 then
-      vim.cmd("b " .. layout[3])
+  if layout.type == "leaf" then
+    -- open the previous buffer
+    if vim.fn.bufexists(layout.bufnr) == 1 then
+      vim.cmd("b " .. layout.bufnr)
+    end
+    -- apply window options
+    for opt, val in pairs(layout.win_opts) do
+      if val ~= nil then
+        vim.wo[opt] = val
+      end
     end
   else
     -- split cols or rows, split n-1 times
     local split_method = "rightbelow vsplit"
-    if layout[1] == "col" then
+    if layout.type == "col" then
       split_method = "rightbelow split"
     end
 
     local wins = { vim.fn.win_getid() }
 
-    for i in ipairs(layout[2]) do
+    for i in ipairs(layout.children) do
       if i ~= 1 then
         vim.cmd(split_method)
         table.insert(wins, vim.fn.win_getid())
@@ -55,7 +85,7 @@ local function apply_layout(layout)
     -- recursive into child windows
     for index, win in ipairs(wins) do
       vim.fn.win_gotoid(win)
-      apply_layout(layout[2][index])
+      apply_layout(layout.children[index])
     end
   end
 end
