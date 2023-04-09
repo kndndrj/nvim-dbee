14c6785fd0346969b1928183e45d5e6b68f8c055
Andrej Kenda <andrej.baca14@gmail.com>
2023-04-09 12:08:55 +0200

    use common result builder for redis client

--- dbee/clients/redis.go
+++ dbee/clients/redis.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/kndndrj/nvim-dbee/dbee/clients/common"
 	"github.com/kndndrj/nvim-dbee/dbee/conn"
 	"github.com/redis/go-redis/v9"
 )
@@ -41,14 +42,45 @@ func (c *RedisClient) Query(query string) (conn.IterResult, error) {
 		return nil, err
 	}
 
-	meta := conn.Meta{
-		Query:     query,
-		Timestamp: time.Now(),
+	// parse response
+	var rows []conn.Row
+	switch rpl := resp.(type) {
+	case int64:
+		rows = []conn.Row{{rpl}}
+	case string:
+		rows = []conn.Row{{rpl}}
+	case []any:
+		rows = sliceToRows(rpl, -1)
+	case map[any]any:
+		for k, v := range rpl {
+			rows = append(rows, conn.Row{k, v})
+		}
+	case nil:
+		return nil, errors.New("no reponse from redis")
+	default:
+		return nil, fmt.Errorf("unknown type reponse from redis: %T!\n", rpl)
 	}
 
-	redisRows, err := newRedisRows(resp, meta)
+	// build result
+	max := len(rows) - 1
+	i := 0
+	result := common.NewResultBuilder().
+		WithNextFunc(func() (conn.Row, error) {
+			if i > max {
+				return nil, nil
+			}
+			val := rows[i]
+			i++
+			return val, nil
+		}).
+		WithHeader(conn.Header{"Reply"}).
+		WithMeta(conn.Meta{
+			Query:     query,
+			Timestamp: time.Now(),
+		}).
+		Build()
 
-	return redisRows, err
+	return result, err
 }
 
 func (c *RedisClient) Schema() (conn.Schema, error) {
@@ -61,31 +93,7 @@ func (c *RedisClient) Close() {
 	c.redis.Close()
 }
 
-type RedisRows struct {
-	iter func() conn.Row
-	meta conn.Meta
-}
-
-func newRedisRows(reply any, meta conn.Meta) (*RedisRows, error) {
-	iter, err := getIter(reply)
-	if err != nil {
-		return nil, err
-	}
-
-	return &RedisRows{
-		iter: iter,
-		meta: meta,
-	}, nil
-}
-
-func (r *RedisRows) Meta() (conn.Meta, error) {
-	return r.meta, nil
-}
-
-func (r *RedisRows) Header() (conn.Header, error) {
-	return conn.Header{"Reply"}, nil
-}
-
+// sliceToRows expands []any slice and any possible nested slices to multiple rows
 func sliceToRows(slice []any, level int) []conn.Row {
 	var rows []conn.Row
 
@@ -104,43 +112,4 @@ func sliceToRows(slice []any, level int) []conn.Row {
 		}
 	}
 	return rows
-}
-
-func getIter(redisReply any) (func() conn.Row, error) {
-
-	var rows []conn.Row
-	switch rpl := redisReply.(type) {
-	case int64:
-		rows = []conn.Row{{rpl}}
-	case string:
-		rows = []conn.Row{{rpl}}
-	case []any:
-		rows = sliceToRows(rpl, -1)
-	case map[any]any:
-		for k, v := range rpl {
-			rows = append(rows, conn.Row{k, v})
-		}
-	case nil:
-		return nil, errors.New("no reponse from redis")
-	default:
-		return nil, fmt.Errorf("unknown type reponse from redis: %T!\n", rpl)
-	}
-
-	max := len(rows) - 1
-	i := 0
-	return func() conn.Row {
-		if i > max {
-			return nil
-		}
-		val := rows[i]
-		i++
-		return val
-	}, nil
-}
-
-func (r *RedisRows) Next() (conn.Row, error) {
-	return r.iter(), nil
-}
-
-func (r *RedisRows) Close() {
 }
