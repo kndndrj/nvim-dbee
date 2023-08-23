34b6b978323235ef101bfbb080e1d5dc4d316f8d
Mattias Sjödin <mattias.sjodin.6764@hotmail.com>
2023-08-23 23:17:19 +0200

    refactor: sorting of latest history query

--- lua/dbee/handler/conn.lua
+++ lua/dbee/handler/conn.lua
@@ -219,10 +219,21 @@ function Conn:layout()
       return {}
     end
 
-    -- sort keys
-    table.sort(layout_go, function(k1, k2)
-      return k1.type .. k1.name < k2.type .. k2.name
-    end)
+    -- sort children of keys in layout
+    for _, lgo in ipairs(layout_go) do
+      if lgo.name == "history" then
+        -- sort descendingly, latest query in the top
+        -- due to table.name is epoch time.
+        table.sort(lgo.children, function(k1, k2)
+          return k1.name > k2.name
+        end)
+      elseif lgo.name == "structure" then
+        -- sort ascendingly, alphabetically
+        table.sort(lgo.children, function(k1, k2)
+          return k1.name < k2.name
+        end)
+      end
+    end
 
     local new_layouts = {}
     for _, lgo in ipairs(layout_go) do
