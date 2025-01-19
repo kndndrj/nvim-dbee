1d74cdbcbdecb4f92e0ef6b1d253e749ba52c686
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-01-19 22:46:04 +0100

    feat: add integration test helpers

---
+++ dbee/tests/testhelpers/helper.go
@@ -0,0 +1,119 @@
+// Package testhelpers provides helpers for integration tests.
+package testhelpers
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/kndndrj/nvim-dbee/dbee/core"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+const (
+	// eventBufferTime is a padding to let events come through (e.g. archived)
+	eventBufferTime = 100 * time.Millisecond
+	// eventTimeout is the maximum time to wait for an event to come through
+	eventTimeout = 10 * time.Second
+)
+
+// errTimeOut is an error for when an event did not finish within the expected time.
+var errTimeOut = fmt.Errorf("event did not finish within %v", eventTimeout)
+
+// GetContainerProvider returns the container provider type to use for the tests.
+// If we detect podman is available, we use it, otherwise we use docker.
+func GetContainerProvider() testcontainers.ProviderType {
+	if _, err := exec.LookPath("podman"); err == nil {
+		fmt.Println("Podman detected. Remember to set TESTCONTAINERS_RYUK_CONTAINER_PRIVILEGED=true;")
+		return testcontainers.ProviderPodman
+	}
+	return testcontainers.ProviderDocker
+}
+
+// GetResult is a helper function for calling the Execute method on a driver
+// and waiting for the result to be available.
+func GetResult(t *testing.T, d *core.Connection, query string) ([]core.Row, core.Header, []core.CallState, error) {
+	t.Helper()
+
+	var result *core.Result
+	outStates := make([]core.CallState, 0)
+	outRows := make([]core.Row, 0)
+
+	call := d.Execute(query, func(state core.CallState, c *core.Call) {
+		outStates = append(outStates, state)
+
+		var err error
+		if state == core.CallStateRetrieving {
+			result, err = c.GetResult()
+			assert.NoError(t, err)
+			outRows, err = result.Rows(0, result.Len())
+			assert.NoError(t, err)
+		}
+	})
+
+	select {
+	case <-call.Done():
+		time.Sleep(eventBufferTime)
+		return outRows, result.Header(), outStates, nil
+
+	case <-time.After(eventTimeout):
+		return nil, nil, nil, errTimeOut
+	}
+}
+
+// GetResultWithCancel is a helper function for calling the Execute method on a driver
+// and canceling the call after the first state is received.
+func GetResultWithCancel(t *testing.T, d *core.Connection, query string) (*core.Result, []core.CallState, error) {
+	t.Helper()
+
+	var (
+		outResult *core.Result
+		outErr    error
+	)
+	outStates := make([]core.CallState, 0)
+
+	call := d.Execute(query, func(cs core.CallState, c *core.Call) {
+		outStates = append(outStates, cs)
+		c.Cancel()
+	})
+
+	select {
+	case <-call.Done():
+		time.Sleep(eventBufferTime)
+		return outResult, outStates, outErr
+	case <-time.After(eventTimeout):
+		return nil, nil, errTimeOut
+	}
+}
+
+// GetSchemas returns a list of schema names from the given structure.
+func GetSchemas(t *testing.T, structure []*core.Structure) []string {
+	t.Helper()
+
+	schemas := make([]string, 0)
+	for _, s := range structure {
+		if s.Name == s.Schema {
+			schemas = append(schemas, s.Name)
+			continue
+		}
+	}
+	return schemas
+}
+
+// GetModels returns a list of model names (views, table, etc) from the given structure.
+func GetModels(t *testing.T, structure []*core.Structure, modelType core.StructureType) []string {
+	t.Helper()
+
+	out := make([]string, 0)
+	for _, s := range structure {
+		for _, c := range s.Children {
+			if c.Type == modelType {
+				out = append(out, c.Name)
+				continue
+			}
+		}
+	}
+	return out
+}
