4a70516c185c7ea63e5abae85f33c7cbd2e4a5d8
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-02-05 11:33:39 +0100

    Add schema to driver structure

--- dbee/adapters/duck_driver.go
+++ dbee/adapters/duck_driver.go
@@ -18,36 +18,37 @@ func (c *duckDriver) Query(ctx context.Context, query string) (core.ResultStream
 }
 
 func (c *duckDriver) Columns(opts *core.TableOptions) ([]*core.Column, error) {
-	return c.c.ColumnsFromQuery("DESCRIBE %q", opts.Table)
+	return c.c.ColumnsFromQuery("DESCRIBE %q.%q", opts.Schema, opts.Table)
 }
 
 func (c *duckDriver) Structure() ([]*core.Structure, error) {
-	query := `SHOW TABLES;`
+	catalogQuery := `
+		SELECT table_schema, table_name, table_type
+		FROM information_schema.tables;`
 
-	rows, err := c.Query(context.TODO(), query)
+	rows, err := c.Query(context.Background(), catalogQuery)
 	if err != nil {
 		return nil, err
 	}
 
-	var schema []*core.Structure
-	for rows.HasNext() {
-		row, err := rows.Next()
-		if err != nil {
-			return nil, err
-		}
-
-		// We know for a fact there is only one string field (see query above)
-		table := row[0].(string)
-		schema = append(schema, &core.Structure{
-			Name:   table,
-			Schema: "",
-			Type:   core.StructureTypeTable,
-		})
-	}
-
-	return schema, nil
+	return core.GetGenericStructure(rows, getDuckDBStructureType)
 }
 
+// getDuckDBStructureType returns the core.StructureType based on the
+// given type string for duckdb adapter.
+func getDuckDBStructureType(typ string) core.StructureType {
+	// TODO: (phdah) Add more types if exists
+	switch typ {
+	case "BASE TABLE":
+		return core.StructureTypeTable
+	case "VIEW":
+		return core.StructureTypeView
+	default:
+		return core.StructureTypeNone
+	}
+}
+
+// Close closes the connection to the database.
 func (c *duckDriver) Close() {
 	c.c.Close()
 }
