c4bd9626059b2da938e78b149f94fb22aff797aa
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-02-14 18:11:27 +0100

    review: update struct name

--- dbee/adapters/duck_driver.go
+++ dbee/adapters/duck_driver.go
@@ -18,22 +18,22 @@ type duckDriver struct {
 	currentCatalog string
 }
 
-func (c *duckDriver) Query(ctx context.Context, query string) (core.ResultStream, error) {
-	return c.c.QueryUntilNotEmpty(ctx, query)
+func (d *duckDriver) Query(ctx context.Context, query string) (core.ResultStream, error) {
+	return d.c.QueryUntilNotEmpty(ctx, query)
 }
 
-func (c *duckDriver) Columns(opts *core.TableOptions) ([]*core.Column, error) {
-	return c.c.ColumnsFromQuery("DESCRIBE %q.%q", opts.Schema, opts.Table)
+func (d *duckDriver) Columns(opts *core.TableOptions) ([]*core.Column, error) {
+	return d.c.ColumnsFromQuery("DESCRIBE %q.%q", opts.Schema, opts.Table)
 }
 
-func (c *duckDriver) Structure() ([]*core.Structure, error) {
+func (d *duckDriver) Structure() ([]*core.Structure, error) {
 	catalogQuery := fmt.Sprintf(`
 		SELECT table_schema, table_name, table_type
 		FROM information_schema.tables
 		WHERE table_catalog = '%s';`,
-		c.currentCatalog)
+		d.currentCatalog)
 
-	rows, err := c.Query(context.Background(), catalogQuery)
+	rows, err := d.Query(context.Background(), catalogQuery)
 	if err != nil {
 		return nil, err
 	}
@@ -58,19 +58,18 @@ func getDuckDBStructureType(typ string) core.StructureType {
 // ListDatabases returns the current catalog and a list of available catalogs.
 // NOTE: (phdah) As of now, swapping catalogs is not enabled and only the
 // current will be shown
-func (c *duckDriver) ListDatabases() (current string, available []string, err error) {
+func (d *duckDriver) ListDatabases() (current string, available []string, err error) {
 	// no-op
-	return c.currentCatalog, []string{c.currentCatalog}, nil
+	return d.currentCatalog, []string{d.currentCatalog}, nil
 }
 
 // SelectDatabase switches the current database/catalog to the selected one.
 // NOTE: (phdah) As of now, swapping catalogs is not enabled
-func (c *duckDriver) SelectDatabase(name string) error {
-	// no-op
-	return nil
+func (d *duckDriver) SelectDatabase(name string) error {
+	return fmt.Errorf("SelectDabase method not implemented yet")
 }
 
 // Close closes the connection to the database.
-func (c *duckDriver) Close() {
-	c.c.Close()
+func (d *duckDriver) Close() {
+	d.c.Close()
 }
