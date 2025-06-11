21645e4d88c05d7a58ed97ca72d251cdb6e3a16d
Roman Chesnakov <152219970+romus204@users.noreply.github.com>
2025-06-11 21:08:12 +0700

    fix: edit clickhouse driver query's (#213)

--- dbee/adapters/clickhouse_driver.go
+++ dbee/adapters/clickhouse_driver.go
@@ -36,15 +36,24 @@ func (c *clickhouseDriver) Columns(opts *core.TableOptions) ([]*core.Column, err
 
 func (c *clickhouseDriver) Structure() ([]*core.Structure, error) {
 	query := `
-        SELECT
-            table_schema, table_name, table_type
-            FROM information_schema.tables
-            WHERE lower(table_schema) != 'information_schema'
-        UNION ALL
-        SELECT DISTINCT
-            lower(table_schema), lower(table_name), table_type
-            FROM information_schema.tables
-            WHERE lower(table_schema) = 'information_schema'`
+		SELECT
+			database AS table_schema,
+			name AS table_name,
+			CASE
+				WHEN engine IN (
+					'MergeTree', 'ReplacingMergeTree', 'SummingMergeTree', 'AggregatingMergeTree',
+					'CollapsingMergeTree', 'VersionedCollapsingMergeTree', 'GraphiteMergeTree',
+					'TinyLog', 'Log', 'StripeLog', 'Memory', 'Buffer', 'Distributed'
+				) THEN 'BASE TABLE'
+				WHEN engine = 'View' THEN 'VIEW'
+				WHEN engine = 'MaterializedView' THEN 'VIEW'
+				WHEN engine = 'LiveView' THEN 'VIEW'
+				WHEN database IN ('system', 'information_schema') THEN 'SYSTEM TABLE'
+				ELSE 'UNKNOWN'
+			END AS table_type
+		FROM system.tables
+		ORDER BY database, name
+		`
 
 	rows, err := c.Query(context.TODO(), query)
 	if err != nil {
@@ -60,9 +69,11 @@ func (c *clickhouseDriver) Close() {
 
 func (c *clickhouseDriver) ListDatabases() (current string, available []string, err error) {
 	query := `
-		SELECT currentDatabase(), schema_name
-        FROM information_schema.schemata
-        WHERE schema_name NOT IN (currentDatabase(), 'INFORMATION_SCHEMA')
+		SELECT
+    		currentDatabase() AS current_db,
+    		name AS schema_name
+		FROM system.databases
+		WHERE name NOT IN (currentDatabase(), 'INFORMATION_SCHEMA')
 	`
 
 	rows, err := c.Query(context.TODO(), query)
